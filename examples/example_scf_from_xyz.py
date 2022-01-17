62e962c388a1c0930d98971e3a16945b091f9525
q-posev <posenitskiy@irsamc.ups-tlse.fr>
2022-01-17 10:48:55 +0100

    clean scf_from_xyz example

--- examples/example_scf_from_xyz.py
+++ examples/example_scf_from_xyz.py
@@ -58,28 +58,21 @@ def test_run_create_ezfio(code, computer):
     # COMPILE THE DICTIONARY OF QP2 PARAMETERS
     create_parameters = {
         'qp_create_ezfio': {
-            #'basis': '"6-31g"',
-            'basis': '"aug-cc-pvtz"',
+            'basis': '"6-31g"',
+            #'basis': '"aug-cc-pvtz"',
             'charge': '0',
             'output': EZFIO_NAME,
         },
-        'xyz': XYZ_FILE,
-        'wf_basename': EZFIO_NAME
+        'xyz': XYZ_FILE
     }
 
-    builder.metadata.options.output_filename = 'qp.out'
     builder.metadata.options.output_wf_basename = EZFIO_NAME
-
-    #builder.metadata.options.computer = computer.label
+    builder.metadata.options.output_filename = 'qp.out'
 
     # ============== CREATE_EZFIO SPECIFIC PARAMETERS =========== #
     # QP run to create EZFIO database from the XYZ file
     mol = Molecule.from_file(path.join(INPUT_DIR, XYZ_FILE))
-    #print(mol)
     structure = orm.StructureData(pymatgen_molecule=mol)
-    # uncomment to see how Molecule->StructureData conversion modifies the atomic positions
-    #mol2 = structure.get_pymatgen_molecule()
-    #print(mol2)
 
     builder.structure = structure
     builder.parameters = orm.Dict(dict=create_parameters)
@@ -90,19 +83,14 @@ def test_run_create_ezfio(code, computer):
     builder.metadata.computer = computer
 
     # Run the calculation & parse results
-    print('\nQP2 create_ezfio execution: STARTED\n')
+    print('\nQP create_ezfio execution: STARTED\n')
 
     result = engine.run(builder)
 
-    print('\nQP2 create_ezfio execution: FINISHED\n')
+    print('\nQP create_ezfio execution: FINISHED\n')
 
     ezfio_RemoteData = result['output_wavefunction']
-    #path_to_ezfio = ezfio_RemoteData.get_remote_path()
-    #computer_with_ezfio = ezfio_RemoteData.get_computer_name()
-
     ezfio_full_name = ezfio_RemoteData.filename
-    #path_to_ezfio.split('/')[-1]
-    #ezfio_base_name = ezfio_full_name.split('.tar.gz')[0]
 
     print('EZFIO RemoteData name   : ', ezfio_full_name)
     print('EZFIO RemoteData object : ', ezfio_RemoteData)
@@ -114,42 +102,34 @@ def test_run_scf_from_ezfio(code, computer, ezfio_RemoteData_inp):
     """Run JOB #2: SCF calculation on the existing EZFIO database from JOB #1
     """
     builder_scf = code.get_builder()
-    #ezfio_name = 'hcn.ezfio'
 
-    #path_to_ezfio = ezfio_RemoteData_inp.get_remote_path()
     ezfio_full_name = ezfio_RemoteData_inp.filename
-    ezfio_inp_name = ezfio_full_name.split('.tar.gz')[0]
-
-    qp2_commands = [f'set_file {ezfio_inp_name}', 'run scf']
+    ezfio_inp_name = ezfio_full_name.replace('.tar.gz', '')
 
     # COMPILE THE DICTIONARY OF QP2 PARAMETERS
-    qp2_parameters = {
-        'wf_basename': ezfio_inp_name,
-        'qp_commands': qp2_commands
-    }
+    qp_commands = [f'set_file {ezfio_inp_name}', 'run scf']
+
+    qp_parameters = {'qp_commands': qp_commands}
 
     # conventional QP run
-    builder_scf.parameters = orm.Dict(dict=qp2_parameters)
+    builder_scf.parameters = orm.Dict(dict=qp_parameters)
     builder_scf.wavefunction = ezfio_RemoteData_inp
+    builder_scf.metadata.options.output_wf_basename = ezfio_inp_name
 
     builder_scf.metadata.options.output_filename = 'qp.out'
-    builder_scf.metadata.options.output_wf_basename = ezfio_inp_name
-    #builder_scf.metadata.options.computer = computer.label
 
     builder_scf.code = code
     builder_scf.metadata.description = 'Test job submission with the aiida_qp2 plugin to run SCF calculations'
     builder_scf.metadata.computer = computer
 
-    print('\nQP2 run_scf execution: STARTED\n')
+    print('\nQP run_scf execution: STARTED\n')
 
     result = engine.run(builder_scf)
 
-    print('\nQP2 run_scf execution: FINISHED\n')
+    print('\nQP run_scf execution: FINISHED\n')
 
     ezfio_RemoteData = result['output_wavefunction']
-
     ezfio_full_name = ezfio_RemoteData.filename
-    #ezfio_base_name = ezfio_full_name.split('.tar.gz')[0]
 
     print('EZFIO RemoteData name   : ', ezfio_full_name)
     print('EZFIO RemoteData object : ', ezfio_RemoteData)
@@ -179,9 +159,9 @@ def cli():
     Help: $ ./example_scf_fromXYZ.py --help
     """
     (code, computer) = load_aiida_setup()
-    ezfio_RemoteData = test_run_create_ezfio(code, computer)
+    wf1 = test_run_create_ezfio(code, computer)
 
-    energy_scf = test_run_scf_from_ezfio(code, computer, ezfio_RemoteData)
+    energy_scf = test_run_scf_from_ezfio(code, computer, wf1)
     print('\nOutput: SCF energy\n', float(energy_scf))
 
 
