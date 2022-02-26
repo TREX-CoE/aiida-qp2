f1b6d314f595611d5a9a5017e2dc14d9651cdf4f
q-posev <posenitskiy@irsamc.ups-tlse.fr>
2022-02-26 18:23:59 +0100

    Remove the testing framework inherited from the plugin cutter

--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-""" Tests for the plugin.
-
-Includes tests written in pytest style (test_calculations.py).
-"""
-import os
-
-TEST_DIR = os.path.dirname(os.path.realpath(__file__))
