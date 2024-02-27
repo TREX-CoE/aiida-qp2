9f07420b6acf4648017c8c7a877bdd6e8f6bbf67
Otto Kohulak <pravod@gmail.com>
2024-02-27 09:51:01 +0100

    clean up pk_decorator

--- aiida_qp2/cli_helpers.py
+++ aiida_qp2/cli_helpers.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Helper functions for the CLI
+"""
+
 import click
 
 from aiida.orm import load_node, Group, SinglefileData, QueryBuilder
@@ -7,34 +12,38 @@ from functools import wraps
 _PQ_GROUP = "pq2_project_group"
 
 def pk_decorator(func):
+    """
+    Decorator add click option `--pk` to the function
+    if `--pk` is not provided, the function will try to find
+    the active project and use the latest wavefunction
+    """
+
     @click.option("--wavefunction", "-w", type=click.STRING, default=None, help="Wavefunction to use")
     @wraps(func)
     def f(*_args, **kwargs):
         if kwargs["wavefunction"] is not None:
             from aiida.orm import load_node
-            kwargs["wavefunction"] = load_node(int(kwargs["wavefunction"]))
+            kwargs["wavefunction"] = load_node(kwargs["wavefunction"])
         else:
             from aiida.orm import QueryBuilder, Group, SinglefileData as Wavefunction
-            qb = QueryBuilder()
-            qb.append(Group, filters={"label": _PQ_GROUP}, tag="group")
-            group, = qb.first()
-
             qb = QueryBuilder()
             qb.append(Group, filters={"label": _PQ_GROUP}, tag="group")
             qb.append(Wavefunction, filters={ 'id': group.base.extras.all['active_project']},
                                     with_group="group",
                                     tag="wavefunction")
-            if qb.count() == 1:
+            if qb.count() > 0:
                 wavefunction, = qb.first()
+                qb = QueryBuilder()
+                qb.append(Wavefunction, filters={ 'id': wavefunction.pk}, tag="mother")
+                qb.append(Wavefunction, with_ancestors="mother", tag="child")
+                qb.order_by({"child": {'ctime': 'desc'}})
+                if qb.count() > 0:
+                    kwargs["wavefunction"] = qb.first()[0]
+                else:
+                    kwargs["wavefunction"] = wavefunction
             else:
-                kwargs["pk"] = None
+                kwargs["wavefunction"] = None
 
-            qb = QueryBuilder()
-            qb.append(Wavefunction, filters={ 'id': wavefunction.pk}, tag="mother")
-            qb.append(Wavefunction, with_ancestors="mother", tag="child")
-            qb.order_by({"child": {'ctime': 'desc'}})
-            if qb.count() != 0:
-                kwargs["wavefunction"] = qb.first()[0]
 
         return func(*_args, **kwargs)
     return f
